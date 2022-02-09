b03a55b20b6e476d623febb65bd610053a2a1fb8
Sabnock01 <james.thompson1848@gmail.com>
2022-02-09 11:46:22 -0600

    chore: add tailwindcss and make mockup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        colors: {
+            white: "#ffffff",
+            gray: "#273444",
+            black: "#0F172A",
+        },
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}

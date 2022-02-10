1cbd5f13a8ba443cfe939724dd35d9fd0fb41eb5
Sabnock01 <james.thompson1848@gmail.com>
2022-02-09 21:25:21 -0600

    feat: update routers

--- api/routers/project.router.js
+++ api/routers/project.router.js
@@ -2,4 +2,11 @@ const { Router } = require('express');
 
 const router = Router();
 
+router.get('/', controller.getAll);
+router.get('/:userId', controller.getAllForUser);
+router.get('/:id', controller.getById);
+router.post('/', controller.create);
+router.put('/:id', controller.update);
+router.delete('/:id', controller.delete);
+
 module.exports = router;
--- api/routers/user.router.js
+++ api/routers/user.router.js
@@ -2,4 +2,10 @@ const { Router } = require('express');
 
 const router = Router();
 
+router.get('/', controller.getAll);
+router.get('/:id', controller.getById);
+router.post('/', controller.create);
+router.put('/:id', controller.update);
+router.delete('/:id', controller.delete);
+
 module.exports = router;

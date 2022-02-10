1bd228e9d984a80fa6020086614ae799529bde06
Sabnock01 <james.thompson1848@gmail.com>
2022-02-10 11:36:20 -0600

    fix: update docker-compose and create migrations

--- api/routers/project.router.js
+++ api/routers/project.router.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
+const controller = require('../controllers/project.controller');
 
 const router = Router();
 
 router.get('/', controller.getAll);
-router.get('/:userId', controller.getAllForUser);
+// router.get('/:userId', controller.getAllForUser);
 router.get('/:id', controller.getById);
 router.post('/', controller.create);
 router.put('/:id', controller.update);
--- api/routers/task.router.js
+++ api/routers/task.router.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
+const controller = require('../controllers/task.controller');
 
 const router = Router();
 
 router.get('/', controller.getAll);
-router.get('/:userId', controller.getAllForUser);
-router.get('/:projectId', controller.getAllForProject);
+// router.get('/:userId', controller.getAllForUser);
+// router.get('/:projectId', controller.getAllForProject);
 router.get('/:id', controller.getById);
 router.post('/', controller.create);
 router.put('/:id', controller.update);

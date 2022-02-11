1ae85c0183fc1300402288563bd9d483c2d44c9c
Sabnock01 <james.thompson1848@gmail.com>
2022-02-10 21:08:47 -0600

    feat: implement user migration

--- api/migrations/20220210173119-create-user.js
+++ api/migrations/20220210173119-create-user.js
@@ -1,21 +1,32 @@
 'use strict';
 
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
+const tableName = "users";
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+module.exports = {
+  up: async (queryInterface, Sequelize)  => queryInterface.sequelize.transaction (async (t) => {
+    await queryInterface.createTable(tableName, {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false 
+      }, 
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+    }, { transaction: t });
+  }),
+
+  down: async (queryInterface, Sequelize) => queryInterface.sequelize.transaction (async (t) => {
+    await queryInterface.dropTable(tableName, { transaction: t });
+  }),
 };

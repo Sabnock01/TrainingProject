7f91018a30a51870032cd273ad213b28d56fe168
Sabnock01 <james.thompson1848@gmail.com>
2022-02-10 20:01:56 -0600

    fix: correct db config

--- api/config/db.config.js
+++ api/config/db.config.js
@@ -1,25 +1,25 @@
 require('dotenv').config();
 
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+  development: {
+    username: process.env.DB_USERNAME_DEV,
+    password: process.env.DB_PASSWORD_DEV,
+    database: "database_development",
+    host: process.env.DB_HOST_DEV,
+    dialect: "postgres"
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+  test: {
+    username: process.env.DB_USERNAME_TEST,
+    password: process.env.DB_PASSWORD_TEST,
+    database: "database_test",
+    host: process.env.DB_HOST_TEST,
+    dialect: "postgres",
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: "database_production",
+    host: process.env.DB_HOST,
+    dialect: "postgres"
   }
-}
+};

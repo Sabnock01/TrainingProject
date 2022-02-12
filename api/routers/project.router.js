940f4364867307e582b3149a6c3eb3d17988ebc0
Sabnock01 <james.thompson1848@gmail.com>
2022-02-12 17:30:53 -0600

    chore: uncomment remaining routes

--- api/routers/project.router.js
+++ api/routers/project.router.js
@@ -4,7 +4,7 @@ const controller = require('../controllers/project.controller');
 const router = Router();
 
 router.get('/', controller.getAll);
-// router.get('/:userId', controller.getAllForUser);
+router.get('all-for-user/:userId', controller.getAllForUser);
 router.get('/:id', controller.getById);
 router.post('/', controller.create);
 router.put('/:id', controller.update);

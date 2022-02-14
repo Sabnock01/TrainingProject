7d9b3ec22a03fa78f126758a84730f0a7eb66505
Sabnock01 <james.thompson1848@gmail.com>
2022-02-14 11:47:54 -0600

    feat: init vue-router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,35 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import './index.css'
 
+import Login from './components/Login';
+import Main from './components/Main';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/login',
+    component: Login,
+  },
+  {
+    path: '/home',
+    component: Main,
+  }
+]
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+})
+
 Vue.config.productionTip = false
 
 new Vue({
+  router, 
   vuetify,
   render: h => h(App)
 }).$mount('#app')

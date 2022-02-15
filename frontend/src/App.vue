1a89653c26fb09f5d8e073f403df98957bee6430
Sabnock01 <james.thompson1848@gmail.com>
2022-02-14 23:17:14 -0600

    feat: implement nav bar again on new ui

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -34,7 +34,17 @@
             <v-app-bar-nav-icon
                @click.stop="drawer = !drawer;"
             ></v-app-bar-nav-icon>
-            <v-toolbar-title>Application</v-toolbar-title>
+            <div>
+               <div class="bg-black">
+                  <div class="px-4 flex justify-between">
+                  <div>
+                     <v-btn class="mx-1" large color="#F8FAFC" text>Users</v-btn>
+                     <v-btn class="mx-1" large color="#F8FAFC" text>Projects</v-btn>
+                     <v-btn class="mx-1" large color="#F8FAFC" text>Tasks</v-btn>
+                  </div>
+                  </div>
+               </div>
+            </div>
          </v-app-bar>
       </template>
       <v-main>

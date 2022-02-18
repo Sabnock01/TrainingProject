d076e4af353c61cdc27ccf2004b8b0de50caae36
Sabnock01 <james.thompson1848@gmail.com>
2022-02-17 20:46:51 -0600

    feat: implement create buttons

--- frontend/src/components/ProjectsTable.vue
+++ frontend/src/components/ProjectsTable.vue
@@ -1,5 +1,27 @@
 <template>
   <div>
+    <v-container>
+      <v-row>
+        <v-col class="d-flex justify-end">
+          <v-dialog
+            v-model="dialog"
+            width="500"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                v-bind="attrs"
+                v-on="on"
+              >Create Project</v-btn>
+            </template>
+            <v-card>
+              <v-card-title class="text-h5 grey lighten-2">
+                New Project
+              </v-card-title>
+            </v-card>
+          </v-dialog>
+        </v-col>
+      </v-row>
+    </v-container>
     <v-data-table
       :headers="headers"
       :items="projects"
